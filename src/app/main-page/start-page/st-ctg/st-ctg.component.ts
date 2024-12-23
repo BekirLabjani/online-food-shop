aaac3f018a36a6be01518af754dffcca23808c94
unknown <business.life.bekir@gmail.com>
2024-12-23 23:07:20 +0100

    load Prodcts

--- src/app/main-page/start-page/st-ctg/st-ctg.component.ts
+++ src/app/main-page/start-page/st-ctg/st-ctg.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-st-ctg',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, HttpClientModule],
   templateUrl: './st-ctg.component.html',
   styleUrl: './st-ctg.component.scss'
 })
-export class StCtgComponent {
+export class StCtgComponent implements OnInit {
+  products: any[] = [];
+  categoryName: string = '';
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const categoryId = this.route.snapshot.paramMap.get('id');
+
+    if (categoryId) {
+      this.http.get<any>('assets/category.json').subscribe((data) => {
+        const category = data.categories.find((cat: any) => cat.id === +categoryId);
+        if (category) {
+          this.categoryName = category.categoryName;
+          this.products = category.products;
+        }
+      });
+    }
+  }
 }
